e5ffe6f4a3f671864322085cd1064b8716e08cab
DavideCotigliani <davide.cotigliani@gmail.com>
2025-07-17 17:39:24 +0200

    [UPD] Added some style through bootstrap

--- index.php
+++ index.php
@@ -7,29 +7,40 @@ require_once "./functions.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+      <style>
+      html, body {
+        background-color: black;
+        color: white;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
     <title>Document</title>
 </head>
 <body>
-    <h1>I miei album</h1>
-<div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="text-center mb-3">
+        <h1>I miei album</h1>
     </div>
-</div>
-        <ul>
+    <div class="container">
+        <div class="row g-3 flex-wrap">
+            <?php foreach ($dischi as $disco) {
+                ?>
+                 <div class="col-sm-6 col-md-4 col-lg-4">
+                     <div class="card" style="width: 13rem;">
+                          <img src="<?= $disco['urldellacover'] ?>" class="card-img-top">
+                             <div class="card-body">
+                                 <h5 class="card-title"><?= $disco["titolo"]?></h5>
+                                 <p class="card-text"><?= $disco["artista"]?></p>
+                                  <p class="card-text"><?= $disco["annodipubblicazione"]?></p>
+                                   <p class="card-text"><?= $disco["genere"]?></p>
+                             </div>
+                     </div>
+                 </div>
             <?php
-                foreach ($dischi as $disco) {
-                    echo "<li>$disco[titolo]</li>";
-                    echo "<li><em>$disco[artista]</em></li>";
-                    echo "<li><img src=\"{$disco['urldellacover']}\"></li>";
-                    echo "<li>$disco[annodipubblicazione]</li>";
-                    echo "<li>$disco[genere]</li>";
-                }
+            }
 ?>
-        </ul>
-    
+        </div>
+    </div>
 </body>
 </html>
